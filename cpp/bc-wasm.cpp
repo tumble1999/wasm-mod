767be6c8c4f9f914b123f7d6c0ce9e1ff9a39b01
Tumble <tumblegamer@gmail.com>
2020-08-18 15:35:45 +0100

    hmm

--- cpp/bc-wasm.cpp
+++ cpp/bc-wasm.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <emscripten.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 EMSCRIPTEN_KEEPALIVE
 void helloWorld() {
     printf("Hello from C++\n");
 }
+#ifdef __cplusplus
+}
+#endif
